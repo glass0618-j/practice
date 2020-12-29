d3499e1f76911b76ca08ffde6aa2d3453d9f9a3e
Jihye <jee886361@gmail.com>
2020-12-29 22:45:12 +0900

    12월29일

---
+++ FastCampus/src/customer2/CustomerTest.java
@@ -0,0 +1,21 @@
+package customer2;
+
+public class CustomerTest {
+	public static void main(String[] args) {
+		
+		Customer customer = new Customer();
+		customer.buy();
+		customer.sell();
+		customer.order();
+		customer.sayHello();
+		
+		Buy buyer = customer;
+		buyer.buy();
+		buyer.order();
+		
+		Sell seller = customer;
+		seller.sell();
+		seller.order();
+	}
+
+}
