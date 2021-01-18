722fb4d463f1ffce50f992144adcf3622b37f962
Jihye <jee886361@gmail.com>
2021-01-18 23:41:09 +0900

    java.lang페키지2

---
+++ HonGongJa/src/chapter11/sec01/exam25/MathRandomExample.java
@@ -0,0 +1,9 @@
+package chapter11.sec01.exam25;
+
+public class MathRandomExample {
+	public static void main(String[] args) {
+		
+		int num = (int)(Math.random()*6)+1;
+		System.out.println("ÁÖ»çÀ§ ´«: "+num);
+	}
+}
